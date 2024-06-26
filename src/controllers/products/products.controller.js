52298ad3688aad9add668da3907db64ae5c8793d
Misa <58565358+Misael-Venegas@users.noreply.github.com>
2024-06-25 23:14:40 -0600

    cambios productos

--- src/controllers/products/products.controller.js
+++ src/controllers/products/products.controller.js
@@ -3,16 +3,32 @@ const AppError = require('../../helpers/appError')
 
 const registrarProducto = async (req, res, next) => {
     try {
-        console.log(req.body)
-    } catch (error) {
+        //  console.log(req.body)
+        const {
+            nombreProducto,
+            unidad,
+            cantidad,
+            descripcion,
+            precioUnitario,
+            importe,
+            precioVenta,
+            codigoBarras,
+            productoFarmacia
+        } = req.body
 
+        await db.oneOrNone(`INSERT INTO inventario.producto(
+                 nombre_producto, cantidad, unidad, descripcion, precio_unitario, importe, precio_venta, codigo_barras, producto_farmacia)
+                VALUES ( '${nombreProducto}', ${cantidad}, '${unidad}', '${descripcion}', ${precioUnitario}, ${importe}, ${precioVenta}, '${codigoBarras}', ${productoFarmacia})`)
+        return res.status(200).json({ response: 'Ok' })
+    } catch (error) {
+        next(new AppError('Error al intentarn registrar un producto ' + error.message, 500))
     }
 }
 
 const obtenerProductos = async (req, res, next) => {
     try {
         const productos = await db.any(`SELECT *FROM inventario.producto `)
-        console.log(productos)
+
         return res.status(200).json(productos)
     } catch (error) {
         next(new AppError('Error al intentar obtener los productos: ' + error.message, 500))
