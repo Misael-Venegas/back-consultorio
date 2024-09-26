65f60f85b09e431b7eae4e40a3208efbc5f54e59
Misa <misa.cc2393293@gmail.com>
2024-09-25 22:23:01 -0600

    ventas

--- src/controllers/ventas/sales.cotroller.js
+++ src/controllers/ventas/sales.cotroller.js
@@ -5,8 +5,8 @@ const AppError = require('../../helpers/appError')
 const obtenerListaProductosPorCodigoDeBarras = async (req, res, next) => {
     try {
         const { codigoBarras } = req.params
-        const listProductos = await db.any(`SELECT * FROM inventario.producto WHERE (codigo_barras ILIKE %'${codigoBarras}'% OR nombre_producto ILIKE %'${codigoBarras}'%) AND activo = true`)
-        console.log(listProductos)
+        const listProductos = await db.any(`SELECT * FROM inventario.producto WHERE (codigo_barras ILIKE '%${codigoBarras}%' OR nombre_producto ILIKE '%${codigoBarras}%') AND activo = true`)
+        console.log('Pruductos codigo barras ', listProductos)
         return res.status(200).json(listProductos)
     } catch (error) {
         next(new AppError('Error al intentar obtener los productos' + error.message, 500))
--- src/routes/routesWithoutAutentication.js
+++ src/routes/routesWithoutAutentication.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const route = express.Router()
-const { db } = require('../config/bd.config')
-
+const { obtenerListaProductosPorCodigoDeBarras } = require('../controllers/ventas/sales.cotroller')
 const { inicarSesion } = require('../controllers/users/users.controller')
 
 route.post('/login', inicarSesion)
+    .get('/obtener-lista-productos-codigo-barras/:codigoBarras', obtenerListaProductosPorCodigoDeBarras)
 
 module.exports = route
