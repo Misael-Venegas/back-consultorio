2e94a8faf559188ad31a29fc99717b6c2665df45
Misa <misa.cc2393293@gmail.com>
2025-03-19 17:04:30 -0600

    ventas

--- src/controllers/tienda/tienda.controller.js
+++ src/controllers/tienda/tienda.controller.js
@@ -1,5 +1,9 @@
 const AppError = require("../../helpers/appError")
 const { db } = require('../../config/bd.config')
+const Stripe = require('stripe')
+require('dotenv').config();
+
+
 const obtenerProductosTienda = async (req, res, next) => {
     try {
         const respuesta = await db.any(`
@@ -7,11 +11,28 @@ const obtenerProductosTienda = async (req, res, next) => {
                         INNER JOIN inventario.imagenes img ON img.id = pr.id_image
                         WHERE pr.venta_externa = true 
                      `)
-                     
+
         return res.status(200).json(respuesta)
     } catch (error) {
         next(new AppError("Error al intentar obtner los productos"))
     }
 }
 
-module.exports = { obtenerProductosTienda }
+const realizarPagoEnLinea = async (req, res) => {
+    try {
+
+        const stripe = Stripe(process.env.SECRET_STRIPE_KEY)
+
+        const { amount, currency } = req.body;
+
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency,
+        });
+        res.json({ clientSecret: paymentIntent.client_secret });
+    } catch (error) {
+        next(new AppError(error.message))
+    }
+}
+
+module.exports = { obtenerProductosTienda, realizarPagoEnLinea }
--- src/routes/routesWithoutAutentication.js
+++ src/routes/routesWithoutAutentication.js
@@ -2,11 +2,12 @@ const express = require('express')
 const route = express.Router()
 const { obtenerListaProductosPorCodigoDeBarras, realizarVenta } = require('../controllers/ventas/sales.cotroller')
 const { inicarSesion, recuperarContrasenha } = require('../controllers/users/users.controller')
-const { obtenerProductosTienda } = require('../controllers/tienda/tienda.controller')
+const { obtenerProductosTienda, realizarPagoEnLinea } = require('../controllers/tienda/tienda.controller')
 
 route.post('/login', inicarSesion)
     .get('/obtener-lista-productos-codigo-barras/:codigoBarras', obtenerListaProductosPorCodigoDeBarras)
     .post('/realizar-venta', realizarVenta)
     .get('/recuperar-contrasenha/:correoElectronico', recuperarContrasenha)
     .get('/obtener-productos-tienda', obtenerProductosTienda)
+    .post('/create-payment-intent', realizarPagoEnLinea)
 module.exports = route
